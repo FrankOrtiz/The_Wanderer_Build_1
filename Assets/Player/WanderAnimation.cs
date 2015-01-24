597d1d5a25bea5ee4dad7c97e3e2655113dd54af
Tasio Victoria <tasiovictoria@Tasios-MacBook-Pro.local>
2015-01-24 18:51:35 -0500

    main functionality complete

--- Assets/Player/WanderAnimation.cs
+++ Assets/Player/WanderAnimation.cs
@@ -2,30 +2,26 @@
 using System.Collections;
 
 public class WanderAnimation : MonoBehaviour {
-
-	public Sprite[] idle;
-	public Sprite[] movement;
-
-	public float framesPerSecond;
 	
-	private SpriteRenderer spriteRenderer;
 	private Animator anim;
 	
 	// Use this for initialization
 	void Start () {
-		spriteRenderer = renderer as SpriteRenderer;
 		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || 
-		    Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || 
-		    Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || 
-		    Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
-			anim.SetBool("Change", true); 
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+			anim.SetInteger("Direction", 2); 
+		} else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+			anim.SetInteger("Direction", 3); 
+		} else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+			anim.SetInteger("Direction", 0); 
+		} else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+			anim.SetInteger("Direction", 1); 
 		} else {
-			anim.SetBool("Change", false); 
+			anim.SetInteger("Direction", -1); 
 		}
 	}
 }
